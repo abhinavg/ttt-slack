17efc24a330dc5bc3c2594294f5f94b43323c8ab
Abhinav Garg <abhinav.garg@clever.com>
2016-09-25 21:15:11 -0700

    Move code around to add a separate initServer function.

--- index.js
+++ index.js
@@ -6,20 +6,12 @@ const morgan = require('morgan');
 const help = require('./help');
 const models = require('./models');
 
-const app = express();
-const port = process.env.PORT || 3000;
-const slashTTTToken = process.env.SLASH_TTT_TOKEN || '';
-
-app.use(helmet());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(morgan('combined'));
-
-function verifySlackToken(req, res, next) {
-  if (req.body.token === slashTTTToken) {
+const verifySlackToken = token => (req, res, next) => {
+  if (req.body.token === token) {
     return next();
   }
   return res.status(400).send(`Invalid token "${req.body.token}"`);
-}
+};
 
 function verifyCommand(req, res, next) {
   if (req.body.command === '/ttt') {
@@ -28,26 +20,38 @@ function verifyCommand(req, res, next) {
   return res.status(400).send(`Invalid command "${req.body.token}"`);
 }
 
-const verifySlashTTTReq = [verifySlackToken, verifyCommand];
+function initServer(port, token) {
+  const app = express();
+  app.use(helmet());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(morgan('combined'));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+  app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
 
-app.post('/slash/ttt', ...verifySlashTTTReq, (req, res) => {
-  console.log(`Request Body: ${JSON.stringify(req.body)}`);
-  const splitText = (req.body.text || '').split(' ');
-  switch (splitText[0]) {
-    case '':
-    case models.Commands.Help:
-      res.json(help.HelpResponse);
-      break;
-    default:
-      res.json(help.InvalidCmdResponse);
-      break;
-  }
-});
+  const verifySlashTTTReq = [verifySlackToken(token), verifyCommand];
+  app.post('/slash/ttt', ...verifySlashTTTReq, (req, res) => {
+    console.log(`Request Body: ${JSON.stringify(req.body)}`);
+    const splitText = (req.body.text || '').split(' ');
+    switch (splitText[0]) {
+      case '':
+      case models.Commands.Help:
+        res.json(help.HelpResponse);
+        break;
+      default:
+        res.json(help.InvalidCmdResponse);
+        break;
+    }
+  });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
-});
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+  });
+}
+
+if (require.main === module) {
+  const port = process.env.PORT || 3000;
+  const slashTTTToken = process.env.SLASH_TTT_TOKEN || '';
+  initServer(port, slashTTTToken);
+}
