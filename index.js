ade3e0ba4a25fbc299367aabfbe4e9d7353ad5bb
Abhinav Garg <abhinav.garg@clever.com>
2016-09-25 21:32:59 -0700

    Move server code to own module with class

--- index.js
+++ index.js
@@ -3,55 +3,16 @@ const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
-const help = require('./help');
-const models = require('./models');
-
-const verifySlackToken = token => (req, res, next) => {
-  if (req.body.token === token) {
-    return next();
-  }
-  return res.status(400).send(`Invalid token "${req.body.token}"`);
-};
-
-function verifyCommand(req, res, next) {
-  if (req.body.command === '/ttt') {
-    return next();
-  }
-  return res.status(400).send(`Invalid command "${req.body.token}"`);
-}
-
-function initServer(port, token) {
-  const app = express();
-  app.use(helmet());
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(morgan('combined'));
-
-  app.get('/', (req, res) => {
-    res.send('Hello World!');
-  });
-
-  const verifySlashTTTReq = [verifySlackToken(token), verifyCommand];
-  app.post('/slash/ttt', ...verifySlashTTTReq, (req, res) => {
-    console.log(`Request Body: ${JSON.stringify(req.body)}`);
-    const splitText = (req.body.text || '').split(' ');
-    switch (splitText[0]) {
-      case '':
-      case models.Commands.Help:
-        res.json(help.HelpResponse);
-        break;
-      default:
-        res.json(help.InvalidCmdResponse);
-        break;
-    }
-  });
-
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`);
-  });
-}
+const server = require('./server');
 
 if (require.main === module) {
   const port = process.env.PORT || 3000;
   const slashTTTToken = process.env.SLASH_TTT_TOKEN || '';
-  initServer(port, slashTTTToken);
+  const app = express();
+  app.use(helmet());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(morgan('combined'));
+  const tttServer = new server.TTTServer(app, slashTTTToken);
+  tttServer.setupRoutes();
+  tttServer.listen(port);
 }
---
+++ server.js
@@ -0,0 +1,53 @@
+const help = require('./help');
+const models = require('./models');
+
+class TTTServer {
+  constructor(app, token) {
+    this.token = token;
+    this.app = app;
+    this.verifySlackToken = this.verifySlackToken.bind(this);
+    this.setupRoutes = this.setupRoutes.bind(this);
+    this.listen = this.listen.bind(this);
+  }
+
+  static verifyCommand(req, res, next) {
+    if (req.body.command === '/ttt') {
+      return next();
+    }
+    return res.status(400).send(`Invalid command "${req.body.command}"`);
+  }
+
+  verifySlackToken(req, res, next) {
+    if (req.body.token === this.token) {
+      return next();
+    }
+    return res.status(400).send(`Invalid token "${req.body.token}"`);
+  }
+
+  setupRoutes() {
+    const verifySlashTTTReq = [this.verifySlackToken, TTTServer.verifyCommand];
+    this.app.post('/slash/ttt', ...verifySlashTTTReq, (req, res) => {
+      console.log(`Request Body: ${JSON.stringify(req.body)}`);
+      const splitText = (req.body.text || '').split(' ');
+      switch (splitText[0]) {
+        case '':
+        case models.Commands.Help:
+          res.json(help.HelpResponse);
+          break;
+        default:
+          res.json(help.InvalidCmdResponse);
+          break;
+      }
+    });
+  }
+
+  listen(port) {
+    this.app.listen(port, () => {
+      console.log(`Example app listening on port ${port}!`);
+    });
+  }
+}
+
+module.exports = {
+  TTTServer,
+};
